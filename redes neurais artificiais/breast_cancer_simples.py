05b8cf1dc3b75c31403ab719dc19b344c0141384
hppod <hppodgornik@gmail.com>
2019-04-07 15:58:17 -0300

    criando bases para inicio da rede neural

--- redes neurais artificiais/breast_cancer_simples.py
+++ redes neurais artificiais/breast_cancer_simples.py
@@ -5,3 +5,10 @@ Created on Sun Apr  7 15:46:26 2019
 @author: mmpod
 """
 
+import pandas as pd
+
+previsores = pd.read_csv('entradas-breast.csv')
+classe = pd.read_csv('saidas-breast.csv')
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25)

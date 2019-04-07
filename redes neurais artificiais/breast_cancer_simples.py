900b738c9036b56a0cb8a517e9cca777208b4fab
hppod <hppodgornik@gmail.com>
2019-04-07 16:12:35 -0300

    construindo a rede neural

--- redes neurais artificiais/breast_cancer_simples.py
+++ redes neurais artificiais/breast_cancer_simples.py
@@ -12,3 +12,12 @@ classe = pd.read_csv('saidas-breast.csv')
 
 from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classificador = Sequential()
+classificador.add(Dense(units = 16, activation = 'relu', 
+                        kernel_initializer = 'random_uniform', input_dim = 30))
+classificador.add(Dense(units = 1, activation = 'sigmoid'))

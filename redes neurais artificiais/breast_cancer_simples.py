ed622f4ed2fdc8f040cc21c8c5ebd63d9da1d705
hppod <hppodgornik@gmail.com>
2019-04-07 17:04:05 -0300

    visualizando os pesos

--- redes neurais artificiais/breast_cancer_simples.py
+++ redes neurais artificiais/breast_cancer_simples.py
@@ -20,12 +20,27 @@ from keras.layers import Dense
 classificador = Sequential()
 classificador.add(Dense(units = 16, activation = 'relu', 
                         kernel_initializer = 'random_uniform', input_dim = 30))
+classificador.add(Dense(units = 16, activation = 'relu', 
+                        kernel_initializer = 'random_uniform'))
+classificador.add(Dense(units = 16, activation = 'relu', 
+                        kernel_initializer = 'random_uniform'))
 classificador.add(Dense(units = 1, activation = 'sigmoid'))
 
-classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy', 
+otimizador = keras.optimizers.Adam(lr = 0.001, decay = 0.0001, clipvalue = 0.5)
+classificador.compile(optimizer = otimizador, loss = 'binary_crossentropy', 
                       metrics = ['binary_accuracy'])
+
+#classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy', 
+ #                     metrics = ['binary_accuracy'])
 classificador.fit(previsores_treinamento, classe_treinamento,
-                  batch_size = 10, epochs = 100)
+                  batch_size = 10, epochs = 350)
+
+pesos0 = classificador.layers[0].get_weights()
+print(pesos0)
+print(len(pesos0))
+pesos1 = classificador.layers[1].get_weights()
+pesos2 = classificador.layers[2].get_weights()
+pesos3 = classificador.layers[3].get_weights()
 
 previsoes = classificador.predict(previsores_teste)
 previsoes = (previsoes > 0.5)
